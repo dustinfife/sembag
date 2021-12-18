7900ba007b7fcfe8d6577b5d57ab99988ed36fdc
dustinfife <fife.dustin@gmail.com>
2021-12-17 19:14:58 -0500

    Update test_sem.R

--- inst/test_sem.R
+++ inst/test_sem.R
@@ -14,18 +14,22 @@ end - start
 
 results$varimp
 
-
+require(tidyverse)
 start = Sys.time()
-results = flexforest(data=bi, iterations = 100,
-                     formula = bi_mod, fit_function = fit_rf_sem, variable_sampler = variable_sampler_sem,
+results = flexforest(data=bi, iterations = 1000,
+                     formula = bi_mod, fit_function = fit_rf_sem,
+                     variable_sampler = variable_sampler_sem,
                      validation_function = loss_sem)
 end = Sys.time()
 end - start
 
-xd = data.frame(names=names(results$varimp)[1:50] , value=results$varimp[1:50]) %>%
+results[[1]]
+
+xd = data.frame(names=names(results$varimp)[1:50]  , value=results$varimp[1:50])  %>%
   mutate(names=factor(names, levels=paste0("x", 1:50), ordered=T))
 yd = data.frame(names=names(results$varimp)[51:100], value=results$varimp[51:100])%>%
   mutate(names=factor(names, levels=paste0("y", 1:50), ordered=T))
-
+require(flexplot)
 flexplot(value~names, data=xd)
 flexplot(value~names, data=yd)
+
